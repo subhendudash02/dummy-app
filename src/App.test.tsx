c43b1a9096d90bf92a2fee8f3218575262cb746f
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-05-15 10:35:24 +0530

    test change

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import NewPage from '../src/components/NewPage';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/React/i);
+  const linkElement = screen.getByText(/Click Me/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('test2', () => {
+  render(<NewPage />);
+  const linkElement = screen.getByText(/Hello/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
