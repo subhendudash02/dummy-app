e5bd1b3c24ec8ff61de7a43bf8c94579dd18effd
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-05-17 11:06:14 +0530

    add go back button

--- src/components/NewPage.tsx
+++ src/components/NewPage.tsx
@@ -1,7 +1,16 @@
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+
 function NewPage() {
+    const navigate = useNavigate();
+
+    const routeChange = () => {
+        navigate(-1);
+    }
     return (
         <div className="NewPage">
             <h1>Hello</h1>
+            <Button variant="contained" onClick={ routeChange }>Go Back</Button>
         </div>
     );
 }
