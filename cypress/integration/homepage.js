c2059e8eeefce93cf68f27d1b26c61881bbedbf0
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-05-19 10:40:02 +0530

    add test in homepage.js

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -1,5 +1,13 @@
+// const func = () => {
+//     setTimeout(() => {
+//         cy.get("Button").click();
+//     }, 2000);
+// }
+
 describe("renders the home page", ()=> {
     it("renders correctly", () => {
         cy.visit("/");
+        cy.get("Button").click();
+        cy.get("Button").click();
     })
 })
