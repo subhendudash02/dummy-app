451633a5883df94442e680a864712af3037703b1
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-05-24 23:17:11 +0530

    new testcase for app

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -7,7 +7,18 @@
 describe("renders the home page", ()=> {
     it("renders correctly", () => {
         cy.visit("/");
-        cy.get("Button").click();
-        cy.get("Button").click();
+        if (cy.contains("Click Me")) {
+            cy.log("Button found. Clicking...");
+            cy.wait(2000); // 2s
+            cy.contains("Click Me").click();
+            cy.log("Going Back...");
+            cy.wait(2000); // 2s
+            cy.contains("Go Back").click();
+        }
+        else {
+            cy.log("Button not found. Retrying...");
+        }
+
+
     })
 })
